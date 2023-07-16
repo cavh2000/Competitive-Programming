c0e21884fc95a882b1c771f91a1f6400532ed59f
Carlo Ariel Vargas Hernández <vhcarlo2000@gmail.com>
2023-07-15 20:07:46 -0600

    Create A-StonesOnTheTable.cpp

---
+++ Codeforces/A-StonesOnTheTable.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/problemset/problem/266/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, ans=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=1;i<n;i++){
+        if(s[i]==s[i-1]){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
