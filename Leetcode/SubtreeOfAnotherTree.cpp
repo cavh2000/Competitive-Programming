e04dd0cc41ad70d6bdacc2d66c26ae3a727e1d5d
Carlo Ariel Vargas Hernández <vhcarlo2000@gmail.com>
2023-09-10 21:52:40 -0600

    Create SubtreeOfAnotherTree.cpp

---
+++ Leetcode/SubtreeOfAnotherTree.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/subtree-of-another-tree/
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)
+            return true;
+        if(!p || !q)
+            return false;
+        if(p->val != q->val)
+            return false;
+        
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root || !subRoot)
+            return false;
+        
+        if(isSameTree(root, subRoot))
+            return true;
+        
+        return (isSubtree(root->right, subRoot) || isSubtree(root->left, subRoot));
+        
+    }
+   
+};
